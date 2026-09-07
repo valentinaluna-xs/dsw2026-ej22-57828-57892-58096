a46a1450ddcd24261f7403015afd51fdee2fea80
NachoPaz <82242852+Nacho-Paz@users.noreply.github.com>
2026-09-07 12:06:31 -0300

    Ejercicio 22

--- dashboard.js
+++ dashboard.js
@@ -4,4 +4,16 @@ document.addEventListener('DOMContentLoaded', () => {
   logoutButton.addEventListener('click', () => {
     window.location.href = 'login.html';
   });
+
+  const menuButton = document.getElementById('menu-btn');
+  const sidebar = document.getElementById('sidebar');
+
+  menuButton.addEventListener('click', () => {
+
+    // Solo funciona en modo móvil
+    if (window.innerWidth < 600) {
+      sidebar.classList.toggle('open');
+    }
+
+  });
 });
